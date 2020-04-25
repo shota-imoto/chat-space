a694466431c7bd19493a6e6c79e71bfb9f8115b0
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-04-25 12:32:49 +0900

    add_ajax_begin_controller-function
    # What
    - ajax 発火時にコントローラ＃createに値を引き渡す機能を追加
    
    # Why
    - createアクションを動作させるため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,16 @@
 $(function () {
   $("#new_message").on("submit", function (e) {
     e.preventDefault();
-    console.log("あじゃぱ");
+    console.log($(this).attr("action"));
+    var formData = new FormData(this);
+    url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false,
+    });
   });
 });
