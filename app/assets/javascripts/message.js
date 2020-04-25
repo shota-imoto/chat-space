9bfb2818df0be9ceb33ff41a184bdc34ef39be6b
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-04-25 13:38:38 +0900

    add_route-for-json_to_controller
    # what
    - コントローラにjson形式のデータが送られてきたときの動作を記述
    
    # why
    - jsonデータをjbuilderに転送するため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,9 @@
 $(function () {
   $("#new_message").on("submit", function (e) {
     e.preventDefault();
-    console.log($(this).attr("action"));
+    console.log(this);
     var formData = new FormData(this);
-    url = $(this).attr("action");
+    var url = $(this).attr("action");
     $.ajax({
       url: url,
       type: "POST",
