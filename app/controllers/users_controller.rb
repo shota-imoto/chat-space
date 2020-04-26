eaf44e5c270754d15242976eba1740dcd62b5a86
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-04-26 20:22:11 +0900

    add_ajax-function_to_group-editer
    ## what
     グループ編集画面に非同期通信機能を追加
    - ユーザーのインクリメンタルサーチ
    - ユーザーを選択するとメンバー一覧に選択されたユーザが表示
    - メンバー一覧の削除ボタンを押すとメンバー一覧からユーザー名が削除される
    
    ## why
    グループ作成画面のUI操作性向上のため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
-    respond_to do |form|
+    return nil if params[:keyword] == ""
+    @users = User.where(["name LIKE ?", "%#{params[:keyword]}%"]).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
       format.html
       format.json
     end
@@ -11,6 +13,7 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(current_user.id)
+    binding.pry
     if user.update(user_params)
       redirect_to root_path
     else
