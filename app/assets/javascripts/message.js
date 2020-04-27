8b2463cbec70ff41b93771e56a077d496d0bd818
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-04-27 19:22:48 +0900

    add_function_auto-reload
    ## what
    チャット画面の自動更新機能を追加
    
    ## why
    チャットのリアルタイム性を向上するため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,38 @@
 $(function () {
+  // 自動更新
+  var reloadMessages = function () {
+    var last_message_id = $(".chat-space__message-list__message-box:last").data(
+      "message-id"
+    );
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: { id: last_message_id },
+    })
+      .done(function (messages) {
+        if (messages.length !== 0) {
+          var insertHTML = "";
+          $.each(messages, function (i, message) {
+            insertHTML += buildHTML(message);
+          });
+          $(".chat-space__message-list").append(insertHTML);
+          $(".chat-space__message-list").animate({
+            scrollTop: $(".chat-space__message-list")[0].scrollHeight,
+          });
+        }
+      })
+      .fail(function () {
+        alert("error");
+      });
+  };
+
+  // インクリメンタルサーチ
   function buildHTML(message) {
     if (message.image) {
-      var html = `<div class="chat-space__message-list__message-box">
-        <div class="chat-space__message-list__message-box__title"><div class="chat-space__message-list__message-box__title--name">
+      var html = `<div class="chat-space__message-list__message-box" data-message-id=${message.id}>
+        <div class="chat-space__message-list__message-box__title">
+        <div class="chat-space__message-list__message-box__title--name">
         ${message.user_name}
         </div>
         <div class="chat-space__message-list__message-box__title--created_at">
@@ -15,7 +45,7 @@ $(function () {
         </div>
         </div>`;
     } else {
-      var html = `<div class="chat-space__message-list__message-box">
+      var html = `<div class="chat-space__message-list__message-box" data-message-id=${message.id}>
         <div class="chat-space__message-list__message-box__title"><div class="chat-space__message-list__message-box__title--name">
           ${message.user_name}
         </div><div class="chat-space__message-list__message-box__title--created_at">
@@ -33,7 +63,6 @@ $(function () {
   $("#new_message").on("submit", function (e) {
     e.preventDefault();
     var formData = new FormData(this);
-
     var url = $(this).attr("action");
 
     $.ajax({
@@ -57,4 +86,9 @@ $(function () {
         alert("メッセージ送信に失敗しました");
       });
   });
+
+  // 自動更新関数を7秒ごとに呼び出し
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
